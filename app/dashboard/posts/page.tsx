41e0bbc5e2f9296aaed83d4aebe601533575b5af
Rinat Musin <rinatm@u.yale-nus.edu.sg>
2024-03-31 15:05:02 +0800

    frontend overhaul

--- app/dashboard/posts/page.tsx
+++ app/dashboard/posts/page.tsx
@@ -7,39 +7,43 @@ import { PostsTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchPostsPages } from '@/app/lib/data';
 import { Metadata } from 'next';
- 
-export const metadata: Metadata = {
-  title: 'Posts',
-};
- 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchPostsPages(query);
 
-  return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Posts</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search posts..." />
-        <CreatePost />
-      </div>
-      <p className="mt-4 text-gray-600">This page displays a list of rideshare posts. You can search for specific posts using the search bar above. You may also create posts by clicking on the plus sign.</p>
-       <Suspense key={query + currentPage} fallback={<PostsTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense>
-      <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
-      </div>
-    </div>
-  );
+export const metadata: Metadata = {
+	title: 'Posts',
+};
+
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string;
+		page?: string;
+	};
+}) {
+	const query = searchParams?.query || '';
+	const currentPage = Number(searchParams?.page) || 1;
+	const totalPages = await fetchPostsPages(query);
+
+	return (
+		<div className='w-full'>
+			<div className='hidden md:flex w-full items-center justify-between'>
+				<h1 className={`${lusitana.className} text-2xl`}>Posts</h1>
+			</div>
+			<div className='flex items-center justify-between gap-2 md:mt-8 bg-white mx-[-8px] px-[18px] pb-4'>
+				<Search placeholder='Search posts...' />
+				<CreatePost />
+			</div>
+			<Suspense
+				key={query + currentPage}
+				fallback={<PostsTableSkeleton />}>
+				<Table
+					query={query}
+					currentPage={currentPage}
+				/>
+			</Suspense>
+			<div className='mt-5 flex w-full justify-center'>
+				<Pagination totalPages={totalPages} />
+			</div>
+		</div>
+	);
 }
--- app/ui/posts/buttons.tsx
+++ app/ui/posts/buttons.tsx
@@ -1,40 +1,43 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import {
+	PencilIcon,
+	PlusIcon,
+	TrashIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deletePost } from '@/app/lib/actions';
 
 export function CreatePost() {
-  return (
-    <Link
-      href="/dashboard/posts/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-    >
-      <span className="hidden md:block">Create Post</span>{' '}
-      <PlusIcon className="h-5 md:ml-4" />
-    </Link>
-  );
+	return (
+		<Link
+			href='/dashboard/posts/create'
+			className='flex h-10 items-center rounded-lg bg-green-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'>
+			<span className='hidden md:block'>Create Post</span>{' '}
+			<PlusIcon className='h-5 md:ml-4' />
+		</Link>
+	);
 }
 
 export function UpdatePost({ id }: { id: string }) {
-  return (
-    <Link
-      href={`/dashboard/posts/${id}/edit`}
-      className="rounded-md border p-2 hover:bg-gray-100"
-    >
-      <PencilIcon className="w-5" />
-    </Link>
-  );
+	return (
+		<Link
+			href={`/dashboard/posts/${id}/edit`}
+			className='rounded-md border p-2 hover:bg-gray-100 w-full md:w-auto'>
+			<PencilIcon className='w-5 mx-auto' />
+		</Link>
+	);
 }
 
 export function DeletePost({ id }: { id: string }) {
+	const deletePostWithId = deletePost.bind(null, id);
 
-  const deletePostWithId = deletePost.bind(null, id);
-
-  return (
-    <form action={deletePostWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </form>
-  );
+	return (
+		<form
+			action={deletePostWithId}
+			className='w-full md:w-auto'>
+			<button className='rounded-md border p-2 hover:bg-gray-100 w-full'>
+				<span className='sr-only'>Delete</span>
+				<TrashIcon className='w-5 mx-auto' />
+			</button>
+		</form>
+	);
 }
